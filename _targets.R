763c6d303403a863469d6ebca3f5d8369002bf93
Andrew Heiss <andrew@andrewheiss.com>
2021-09-09 18:57:42 -0400

    pandocification

--- _targets.R
+++ _targets.R
@@ -2,6 +2,8 @@ library(targets)
 library(tarchetypes)
 
 # General pipeline settings -----------------------------------------------
+csl <- "pandoc/csl/apa.csl"
+bibstyle <- "bibstyle-apa"
 
 options(tidyverse.quiet = TRUE,
         dplyr.summarise.inform = FALSE)
@@ -12,6 +14,7 @@ set.seed(4393)  # From random.org
 # R functions -------------------------------------------------------------
 
 # source("R/whatever.R")
+source("R/funs_knitting.R")
 
 # here::here() returns an absolute path, which then gets stored in tar_meta and
 # becomes computer-specific (i.e. /Users/andrew/Research/blah/thing.Rmd).
@@ -24,6 +27,65 @@ here_rel <- function(...) {fs::path_rel(here::here(...))}
 # Pipeline ----------------------------------------------------------------
 
 list(
+
+  ## Manuscript targets ------------------------------------------------------
+
+  # tarchetypes::tar_render() automatically detects target dependencies in Rmd
+  # files and knits them, but there's no easy way to pass a custom rendering
+  # script like bookdown::html_document2(), so two things happen here:
+  #   1. Set a file-based target with tar_target_raw() and use tar_knitr_deps()
+  #      to detect the target dependencies in the Rmd file
+  #   2. Use a bunch of other file-based targets to actually render the document
+  #      through different custom functions
+  tar_target(bib_file,
+             here_rel("manuscript", "bibliography.bib"),
+             format = "file"),
+  
+  tar_target_raw("main_manuscript", here_rel("manuscript", "manuscript.Rmd"),
+                 format = "file",
+                 deps = c("bib_file",
+                          tar_knitr_deps(here_rel("manuscript", "manuscript.Rmd")))),
+  tar_target(html,
+             render_html(
+               input = main_manuscript,
+               output = here_rel("manuscript", "output", "manuscript.html"),
+               csl = csl,
+               bib_file,
+               support_folder = "output/html-support"),
+             format = "file"),
+  tar_target(pdf,
+             render_pdf(
+               input = main_manuscript,
+               output = here_rel("manuscript", "output", "manuscript.pdf"),
+               bibstyle = bibstyle,
+               bib_file),
+             format = "file"),
+  tar_target(ms_pdf,
+             render_pdf_ms(
+               input = main_manuscript,
+               output = here_rel("manuscript", "output", "manuscript-ms.pdf"),
+               bibstyle = bibstyle,
+               bib_file),
+             format = "file"),
+  tar_target(docx,
+             render_docx(
+               input = main_manuscript,
+               output = here_rel("manuscript", "output", "manuscript.docx"),
+               csl = csl,
+               bib_file),
+             format = "file"),
+  tar_target(bib,
+             extract_bib(
+               input_rmd = main_manuscript,
+               input_bib = bib_file,
+               output = here_rel("manuscript", "output", "extracted-citations.bib")),
+             format = "file"),
+  
+  # Always show a word count
+  tar_target(word_count, count_words(html)),
+  tar_force(show_word_count, print(word_count), TRUE),
+  
   # Knit the README
-  tar_render(readme, here_rel("README.Rmd"), output_options = list(html_preview = FALSE))
+  tar_render(readme, here_rel("README.Rmd"),
+             output_options = list(html_preview = FALSE))
 )
