84263f7a1e48d7cd927d6f7955b12fbd0153dabb
Andrew Heiss <andrew@andrewheiss.com>
2023-12-14 20:47:55 -0500

    Massive Quarto-ification

---
+++ R/funs_graphics.R
@@ -0,0 +1,148 @@
+# Windows fonts
+register_fonts <- function() {
+  if (Sys.info()["sysname"] == "Windows") {
+    # Register these fonts if running R on Windows
+    windowsFonts(`Assistant` = windowsFont("Assistant"))
+    windowsFonts(`Assistant Light` = windowsFont("Assistant Light"))
+    windowsFonts(`Assistant SemiBold` = windowsFont("Assistant SemiBold"))
+  }
+}
+
+
+# ggplot themes -----------------------------------------------------------
+set_annotation_fonts <- function() {
+  ggplot2::update_geom_defaults(
+    "label",
+    list(family = "Assistant", fontface = "bold", color = "black")
+  )
+  ggplot2::update_geom_defaults(
+    "text",
+    list(family = "Assistant", fontface = "bold", color = "black")
+  )
+
+  if (isNamespaceLoaded("ggdag")) {
+    ggplot2::update_geom_defaults(
+      ggdag:::GeomDagText,
+      list(family = "Assistant", fontface = "bold", color = "black")
+    )
+  }
+}
+
+#' theme_np
+#'
+#' A custom ggplot2 theme used throughout this project
+#'
+#' @param base_size base font size (default is 11)
+#' @param base_family base font family (default is Assistant)
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+#'   geom_point() +
+#'   labs(x = "Engine displacement", y = "Highway miles per gallon",
+#'        title = "A standard graph about cars",
+#'        subtitle = "Heavier cars get lower mileage",
+#'        caption = "Source: A dataset included in ggplot2",
+#'        color = "Drive") +
+#'   theme_np()
+theme_np <- function(base_size = 11, base_family = "Assistant") {
+  theme_bw(base_size, base_family) + 
+    theme(plot.title = element_text(size = rel(1.4), face = "bold",
+                                    family = "Assistant"),
+          plot.subtitle = element_text(size = rel(1), face = "plain",
+                                       family = "Assistant Light"),
+          plot.caption = element_text(size = rel(0.8), color = "grey50", face = "plain",
+                                      family = "Assistant Light",
+                                      margin = margin(t = 10), hjust = 0),
+          plot.tag = element_text(size = rel(1.4), face = "bold",
+                                  family = "Assistant"),
+          panel.border = element_rect(color = "grey50", fill = NA, size = 0.15),
+          panel.spacing = unit(1, "lines"),
+          panel.grid.minor = element_blank(),
+          strip.text = element_text(size = rel(0.9), hjust = 0,
+                                    family = "Assistant", face = "bold"),
+          strip.background = element_rect(fill = "#ffffff", colour = NA),
+          axis.ticks = element_blank(),
+          axis.title = element_text(family = "Assistant SemiBold", face = "plain", 
+                                    size = rel(0.8)),
+          axis.title.x = element_text(margin = margin(t = 5)),
+          axis.text = element_text(family = "Assistant Light", face = "plain"),
+          legend.key = element_blank(),
+          legend.title = element_text(size = rel(0.8),
+                                      family = "Assistant SemiBold", face = "plain"),
+          legend.text = element_text(size = rel(0.8), 
+                                     family = "Assistant Light", face = "plain"),
+          legend.spacing = unit(0.1, "lines"),
+          legend.box.margin = margin(t = -0.25, unit = "lines"),
+          legend.margin = margin(t = 0),
+          legend.position = "bottom")
+}
+
+#' theme_np_dag
+#'
+#' A custom ggplot2 theme used for DAGs
+#'
+#' @param ... arguments passed to `theme_np()`
+#'
+#' @export
+#' @md
+#'
+#' @examples
+#' library(ggdag)
+#'
+#' ggdag(confounder_triangle()) +
+#'   theme_np_dag()
+theme_np_dag <- function(...) {
+  theme_np(...) + 
+    theme(axis.text = element_blank(), 
+          axis.title = element_blank(), 
+          axis.title.x = element_blank(), 
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(), 
+          panel.border = element_blank(),
+          panel.background = element_blank())
+}
+
+
+# Colors ------------------------------------------------------------------
+
+clrs <- viridisLite::turbo(6, begin = 0, end = 1) |> 
+  purrr::set_names(c("purple", "blue", "green", "yellow", "orange", "red")) |> 
+  as.list()
+
+#' Convert colors to LaTeX RGB definitions
+#'
+#' Take a named list of hex colors and convert them to RGB values.
+#'
+#' The resulting output has the class `clrs` and will print only the LaTeX color
+#' definitions by default using `print.clrs()`
+#'
+#' @param clrs named list of hex colors
+#'
+#' @return A tibble with columns for the color name, hex code, red value, green
+#'   value, blue value, comma-separated RGB value, and a LaTeX color definition
+#'
+#' @export
+#' @md
+#' 
+#' @example 
+#' get_latex_rgb()
+get_latex_rgb <- function(clrs) {
+  clrs <- unlist(clrs)
+  
+  clrs_table <- tibble::enframe(x, name = "color_name", value = "hex") |> 
+    bind_cols(as_tibble(t(col2rgb(x)))) |>
+    mutate(rgb = paste(red, green, blue, sep = ", ")) |>
+    mutate(latex = glue::glue("\\definecolor{<<color_name>>}{RGB}{<<rgb>>}",
+                              .open = "<<", .close = ">>"))
+  
+  class(clrs_table) <- append("color_table", class(clrs_table))
+  clrs_table
+}
+
+print.color_table <- function(x) {
+  cat(x$latex, sep = "\n")
+}
--- _targets.R
+++ _targets.R
@@ -2,19 +2,22 @@ library(targets)
 library(tarchetypes)
 
 # General pipeline settings -----------------------------------------------
-csl <- "pandoc/csl/apa.csl"
-bibstyle <- "bibstyle-apa"
-
-options(tidyverse.quiet = TRUE,
-        dplyr.summarise.inform = FALSE)
+options(
+  tidyverse.quiet = TRUE,
+  dplyr.summarise.inform = FALSE
+)
 
 set.seed(4393)  # From random.org
 
+# Global target options
+tar_option_set(
+  packages = c("tidyverse"),  # Packages available to all targets
+  format = "qs",  # Storage format
+  workspace_on_error = TRUE  # Automatically create a debug workspace on errors
+)
+
 
 # R functions -------------------------------------------------------------
-
-# source("R/whatever.R")
-source("R/funs_knitting.R")
 
 # here::here() returns an absolute path, which then gets stored in tar_meta and
 # becomes computer-specific (i.e. /Users/andrew/Research/blah/thing.Rmd).
@@ -23,72 +26,27 @@ source("R/funs_knitting.R")
 # https://github.com/r-lib/here/issues/36#issuecomment-530894167)
 here_rel <- function(...) {fs::path_rel(here::here(...))}
 
+# Load all the scripts in the R/ folder that contain the functions to be used in
+# the pipeline
+tar_source()
+
 
 # Pipeline ----------------------------------------------------------------
 
 list(
   
-  # Define helper functions
-  tar_target(plot_funs, here_rel("R", "graphics.R"), format = "file"),
+  ## Helper functions ----
+  tar_target(graphic_functions, lst(
+    register_fonts, set_annotation_fonts,  
+    theme_np, theme_np_dag, 
+    clrs, get_latex_rgb, print.color_table)
+  ),
 
-  ## Manuscript targets ------------------------------------------------------
+  ## Manuscript targets ----
 
-  # tarchetypes::tar_render() automatically detects target dependencies in Rmd
-  # files and knits them, but there's no easy way to pass a custom rendering
-  # script like bookdown::html_document2(), so two things happen here:
-  #   1. Set a file-based target with tar_target_raw() and use tar_knitr_deps()
-  #      to detect the target dependencies in the Rmd file
-  #   2. Use a bunch of other file-based targets to actually render the document
-  #      through different custom functions
-  tar_target(bib_file,
-             here_rel("manuscript", "bibliography.bib"),
-             format = "file"),
+  # tar_quarto(manuscript_nice, path = "manuscript", quiet = FALSE, profile = "nice"),
+  # tar_quarto(manuscript_manuscripty, path = "manuscript", quiet = FALSE, profile = "ms"),
   
-  tar_target_raw("main_manuscript", here_rel("manuscript", "manuscript.Rmd"),
-                 format = "file",
-                 deps = c("bib_file",
-                          tar_knitr_deps(here_rel("manuscript", "manuscript.Rmd")))),
-  tar_target(html,
-             render_html(
-               input = main_manuscript,
-               output = here_rel("manuscript", "output", "manuscript.html"),
-               csl = csl,
-               bib_file,
-               support_folder = "output/html-support"),
-             format = "file"),
-  tar_target(pdf,
-             render_pdf(
-               input = main_manuscript,
-               output = here_rel("manuscript", "output", "manuscript.pdf"),
-               bibstyle = bibstyle,
-               bib_file),
-             format = "file"),
-  tar_target(ms_pdf,
-             render_pdf_ms(
-               input = main_manuscript,
-               output = here_rel("manuscript", "output", "manuscript-ms.pdf"),
-               bibstyle = bibstyle,
-               bib_file),
-             format = "file"),
-  tar_target(docx,
-             render_docx(
-               input = main_manuscript,
-               output = here_rel("manuscript", "output", "manuscript.docx"),
-               csl = csl,
-               bib_file),
-             format = "file"),
-  tar_target(bib,
-             extract_bib(
-               input_rmd = main_manuscript,
-               input_bib = bib_file,
-               output = here_rel("manuscript", "output", "extracted-citations.bib")),
-             format = "file"),
-  
-  # Always show a word count
-  tar_target(word_count, count_words(html)),
-  tar_force(show_word_count, print(word_count), TRUE),
-  
-  # Knit the README
-  tar_render(readme, here_rel("README.Rmd"),
-             output_options = list(html_preview = FALSE))
+  ## Render the README ----
+  tar_quarto(readme, here_rel("README.qmd"))
 )
