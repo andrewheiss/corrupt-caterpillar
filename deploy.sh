c3c24398403347b2126fa01d91a94b04492ba6d3
Andrew Heiss <andrew@andrewheiss.com>
2024-11-21 16:48:53 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+REMOTE_HOST="ath-cloud"
+REMOTE_DIR="~/sites/stats.andrewheiss.com/public_html/corrupt-caterpillar"
+REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
+
+echo "Uploading new changes to remote server..."
+echo
+rsync -crvP --exclude '*_cache' --delete _site/ $REMOTE_DEST
